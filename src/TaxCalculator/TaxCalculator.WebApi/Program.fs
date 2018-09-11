96740fe6bea16e82820a134077a7f10ca787cdfd
dsemenov87 <semenovda@modulbank.com>
2018-09-11 12:04:37 +0700

    add vostok

--- src/TaxCalculator/TaxCalculator.WebApi/Program.fs
+++ src/TaxCalculator/TaxCalculator.WebApi/Program.fs
@@ -1,6 +1,7 @@
 ﻿open System
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.Logging
 
 open Giraffe
@@ -12,6 +13,14 @@ open TaxCalculator.DataLayer
 open TaxCalculator.BusinessLogic.Common
 open FsConfig
 open Microsoft.Extensions.Caching.Distributed
+
+open Vostok.Instrumentation.AspNetCore
+open Vostok.Hosting
+open Vostok.Metrics
+open Serilog
+open Serilog.Events
+open Vostok.Logging
+open Vostok.Logging.Serilog
 
 // ---------------------------------
 // Prelude
@@ -33,7 +42,7 @@ type RedisConfig =
     }
 
 type TaxCalculatorConfig =
-    {   LogLevel: LogLevel
+    {   LogLevel: LogEventLevel
         DbName  : string
     }
 
@@ -52,82 +61,114 @@ let config =
         | BadValue (envVarName, value)  -> failwithf "Environment variable %s has invalid value %s" envVarName value
         | NotSupported msg              -> failwith msg
 
+// ---------------------------------
+// Telemetry
+// ---------------------------------
+
+let private createHostLog() : ILog =
+    let loggerConfiguration =
+        LoggerConfiguration()
+            .MinimumLevel.Debug()
+
+    loggerConfiguration
+        .WriteTo.Console(
+            config.TaxCalculator.LogLevel,
+            "{Timestamp:HH:mm:ss.fff} {Level:u3} [{Thread}] {Message:l}{NewLine}{Exception}") |> ignore
+
+    let logger = loggerConfiguration.CreateLogger()
+
+    in SerilogLog(logger) :> ILog
+        
+
+let configureApplication(app : Microsoft.AspNetCore.Builder.IApplicationBuilder) =
+    // ---------------------------------
+    // Composition root
+    // ---------------------------------
+    let storageService =
+        TaxStorageService(
+            TaxDatabaseSettings(config.Pg.Host, config.TaxCalculator.DbName),
+            app.ApplicationServices.GetService<IDistributedCache>())
+
+    let calculatorFactory =
+        TaxCalculatorFactory (storageService, storageService)
+
+    // ---------------------------------
+    // Error handler
+    // ---------------------------------
+
+    let errorHandler (ex : Exception) _ =
+        clearResponse >=> setStatusCode 500 >=> (ex |> string |> text) // for debug mode
+
+    // ---------------------------------
+    // Web app
+    // ---------------------------------
+    
+    let companyTypeRoute companyType =
+        let paramRouting taxSystem =
+            choose [
+                routef "/%f/%f/%f" (fun (input, output, salary) next ctx ->
+                    task {
+                        let! calculator =
+                            calculatorFactory.CreateCalculator(companyType, taxSystem)
+
+                        let! response =
+                            calculator.Invoke
+                                (CustomerTaxParameters(
+                                    RurMoney (dec input),
+                                    RurMoney (dec output),
+                                    RurMoney (dec salary)))
+                        
+                        return! (response |> json |> ok) next ctx
+                    })
+            ]
+
+        choose [
+            subRoute "/osn" (paramRouting EAccountTaxationSystem.Osn)
+            subRoute "/usn6" (paramRouting EAccountTaxationSystem.Usn6)
+            subRoute "/usn15" (paramRouting EAccountTaxationSystem.Usn15)
+        ]
+
+    let webapp =
+        GET >=> choose [
+            subRoute "/api/ip" (companyTypeRoute ECompanyType.IP)
+            subRoute "/api/ooo" (companyTypeRoute ECompanyType.OOO)
+
+            setStatusCode 404 >=> text "Not found"
+        ]
+
+    app.UseVostok() |> ignore
+    app.UseGiraffeErrorHandler errorHandler |> ignore
+    app.UseGiraffe webapp
+
+type TelemetryApplication() =
+    inherit AspNetCoreVostokApplication()
+
+    override __.OnStarted (hostingEnvironment: IVostokHostingEnvironment) =
+        hostingEnvironment.MetricScope.SystemMetrics(TimeSpan.FromMinutes 1.)
+
+    override __.BuildWebHost (hostingEnvironment: IVostokHostingEnvironment) : IWebHost =
+        WebHostBuilder()
+            .UseKestrel()
+            .AddVostokServices()
+            .Configure(fun app -> configureApplication app |> ignore)
+            .ConfigureServices(fun services ->
+                services.AddGiraffe() |> ignore
+                
+                services.AddDistributedRedisCache(fun options ->
+                    options.Configuration   <- config.Redis.Host
+                    options.InstanceName    <- config.Redis.Instance) |> ignore)
+            .Build()
+
 [<EntryPoint>]
 let main _ =
-    WebHostBuilder()
-        .UseKestrel()
-        .Configure(fun app ->
-            // ---------------------------------
-            // Composition root
-            // ---------------------------------
-            let storageService =
-                TaxStorageService(
-                    TaxDatabaseSettings(config.Pg.Host, config.TaxCalculator.DbName),
-                    app.ApplicationServices.GetService<IDistributedCache>())
-
-            let calculatorFactory =
-                TaxCalculatorFactory (storageService, storageService)
-
-            // ---------------------------------
-            // Error handler
-            // ---------------------------------
-
-            let errorHandler (ex : Exception) (logger : ILogger) =
-                logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
-                clearResponse >=> setStatusCode 500 >=> (ex |> string |> text)
-
-            // ---------------------------------
-            // Web app
-            // ---------------------------------
-            
-            let companyTypeRoute companyType =
-                let paramRouting taxSystem =
-                    choose [
-                        routef "/%f/%f/%f" (fun (input, output, salary) next ctx ->
-                            task {
-                                let! calculator =
-                                    calculatorFactory.CreateCalculator(companyType, taxSystem)
-
-                                let! response =
-                                    calculator.Invoke
-                                        (CustomerTaxParameters(
-                                            RurMoney (dec input),
-                                            RurMoney (dec output),
-                                            RurMoney (dec salary)))
-                                
-                                return! (response |> json |> ok) next ctx
-                            })
-                    ]
-
-                choose [
-                    subRoute "/osn" (paramRouting EAccountTaxationSystem.Osn)
-                    subRoute "/usn6" (paramRouting EAccountTaxationSystem.Usn6)
-                    subRoute "/usn15" (paramRouting EAccountTaxationSystem.Usn15)
-                ]
-
-            let webapp =
-                GET >=> choose [
-                    subRoute "/api/ip" (companyTypeRoute ECompanyType.IP)
-                    subRoute "/api/ooo" (companyTypeRoute ECompanyType.OOO)
-
-                    setStatusCode 404 >=> text "Not found"
-                ]
-
-            app.UseGiraffeErrorHandler errorHandler |> ignore
-            app.UseGiraffe webapp)
-
-        .ConfigureServices(fun services ->
-            services.AddGiraffe() |> ignore
-            
-            services.AddDistributedRedisCache(fun options ->
-                options.Configuration   <- config.Redis.Host
-                options.InstanceName    <- config.Redis.Instance) |> ignore)
-        
-        .ConfigureLogging(fun builder ->
-            builder
-                .AddFilter((=) config.TaxCalculator.LogLevel)
-                .AddConsole()
-                |> ignore)
+    VostokHostBuilder<TelemetryApplication>()
+        .SetServiceInfo("Tax", "TaxCalculator")
+        .ConfigureAppConfiguration(fun configurationBuilder ->
+            configurationBuilder.AddEnvironmentVariables() |> ignore
+            configurationBuilder.AddJsonFile("hostsettings.json") |> ignore)
+        .ConfigureHost(fun hostConfigurator ->
+            hostConfigurator.SetEnvironment("dev") |> ignore
+            hostConfigurator.SetHostLog(createHostLog ()))
         .Build()
         .Run()
     0
