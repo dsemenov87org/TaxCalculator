8b9f1fab1d11f9f0616fd93871b28c1894a7b68a
dsemenov87 <semenovda@modulbank.com>
2018-09-12 19:48:01 +0700

    ensure db

--- src/TaxCalculator/TaxCalculator.DataLayer/Program.cs
+++ src/TaxCalculator/TaxCalculator.DataLayer/Program.cs
@@ -1,20 +1,48 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using FluentMigrator.Runner;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 
 namespace TaxCalculator.DataLayer
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            if (args == null || args.Length < 1)
-            {
-                throw new ArgumentNullException("Database server hostname");
-            }
-
             var services = new ServiceCollection();
+
+            await MigrateUp(
+                services,
+                "localhost", // Environment.GetEnvironmentVariable("PG_HOST"),
+                "taxcalculator"// Environment.GetEnvironmentVariable("TAX_CALCULATOR_DB_NAME")
+                );
+        }
+
+        public static async Task MigrateUp(IServiceCollection services, string host, string dbname)
+        {
+            var connectionBuilder =
+                new NpgsqlConnectionStringBuilder();
+            
+            connectionBuilder.Host = host;
+            connectionBuilder.Username = "postgres";
+            connectionBuilder.Port = 5432;
+
+            using (var conn = new NpgsqlConnection(connectionBuilder.ToString()))
+            using (var cmd = conn.CreateCommand())
+            {
+                await conn.OpenAsync();
+
+                cmd.CommandText = $"SELECT count(*) FROM pg_database WHERE datname = '{dbname}'";
+                var d = await cmd.ExecuteScalarAsync();
+                if ((long)d == 0)
+                {
+                    cmd.CommandText = $@"CREATE DATABASE {dbname} WITH OWNER postgres";
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
 
             var serviceProvider =
                 services
@@ -22,10 +50,7 @@ namespace TaxCalculator.DataLayer
                     .ConfigureRunner(rb =>
                         rb.AddPostgres()
                             .WithGlobalConnectionString(
-                                new TaxDatabaseSettings( 
-                                    Environment.GetEnvironmentVariable("PG_HOST"),
-                                    Environment.GetEnvironmentVariable("TAX_CALCULATOR_DB_NAME")
-                                )
+                                new TaxDatabaseSettings(host, dbname)
                                     .ConnectionStrings
                                     .Select(x => x.ConnectionString)
                                     .First())
@@ -39,7 +64,5 @@ namespace TaxCalculator.DataLayer
 
             runner.MigrateUp();
         }
-
-        public async Task 
     }
 }
