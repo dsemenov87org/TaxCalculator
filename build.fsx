a4861a4d3464c2c043f5a918fdecf545eb33c37b
dsemenov87 <semenovda@modulbank.com>
2018-09-12 21:24:53 +0700

    up

--- build.fsx
+++ build.fsx
@@ -34,7 +34,7 @@ Target.create "PublishApp" (fun _ ->
   for path in !! "./src/**/*.*sproj" do publish path)
 
 Target.create "RunMigrations" (fun _ ->
-  Shell.Exec("dotnet ", "./out/TaxCalculator.DataLayer.dll") |> ignore)
+  Shell.Exec("dotnet ", "--version") |> ignore)
 
 Target.create "IntegrationalTests" (fun _ ->
    for path in !! "./test/*/IntegrationalTests.csproj" do DotNet.test id path)
